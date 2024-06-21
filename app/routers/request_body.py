cb6b5451bf31102ad63dd88d9ba26cdfef21d8cb
Aadarsh <aadarshkumar131@gmail.com>
2024-06-21 19:56:41 +0530

    created json examples for api docs

--- app/routers/request_body.py
+++ app/routers/request_body.py
@@ -6,7 +6,7 @@ router = APIRouter()
 
 class Image(BaseModel):
    url: HttpUrl #Pydantic type
-   name: str
+   name: str = Field(examples=['amazing hill']) #setting extra json schema OR example for a single attribute
 
 class Item(BaseModel):
   name: str
@@ -17,6 +17,22 @@ class Item(BaseModel):
   # image: Image | None = None #using nested Models, eg-  "image": {"url": "http://example.com/baz.jpg","name": "The Foo live"}
   images: list[Image] | None = None # "images": [{"url": "http://example.com/baz.jpg","name": "The Foo live"},{"url": "http://example.com/dave.jpg","name": "The Baz"}]
 
+  #setting examples for users, it will be used in the api docs
+  model_config = {
+          "json_schema_extra": {
+              "examples": [
+                  {
+                      "name": "Foo",
+                      "description": "A very nice Item",
+                      "price": 35.4,
+                      "tax": 3.2,
+                      "tags": ['a','b','c'],
+                      "images": [{"url": "http://example.com/baz.jpg","name": "The Foo live"},
+                                {"url": "http://example.com/dave.jpg","name": "The Baz"}]
+                  }
+              ]
+          }
+      }
 
 @router.post("/items/")
 async def create_item(item: Item):
@@ -35,3 +51,36 @@ async def update_item(item_id: int, item: Item, importance: Annotated[int,Body()
 async def create_multiple_images(images: list[Image]): #Body of pure list with custom type
    return images
 
+@router.post("/items/with_multile_examples")
+async def foo(items: Annotated[Item,Body(
+  # using open_api examples- has the functionality to show multiple examples as per the situation
+  openapi_examples = 
+  {
+    "normal": {
+        "summary": "A normal example",
+        "description": "A **normal** item works correctly.",
+        "value": {
+            "name": "Foo",
+            "description": "A very nice Item",
+            "price": 35.4,
+            "tax": 3.2,
+        },
+    },
+    "converted": {
+        "summary": "An example with converted data",
+        "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
+        "value": {
+            "name": "Bar",
+            "price": "35.4",
+        },
+    },
+    "invalid": {
+        "summary": "Invalid data is rejected with an error",
+        "value": {
+            "name": "Baz",
+            "price": "thirty five point four",
+        },
+    },
+  }
+)]): #Body of pure list with custom type
+   return items
